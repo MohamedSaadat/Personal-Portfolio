25679a4d5e52d65e4e2c0a92a370c47f1ce9893f
MohamedSaadat <mohamedgitsaadat@gmail.com>
2025-12-15 20:35:26 +0200

    navs and tabs is done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,15 @@
 // ------------------ initialization --------------------
+// id
 var themeToggleBTN = document.getElementById("theme-toggle-button");
 var scrollToTopBTN = document.getElementById("scroll-to-top");
-var navLinks = document.querySelectorAll(".nav-links a");
-var sections = document.querySelectorAll("section");
+// querySelector
 var menuBTN = document.querySelector(".mobile-menu");
 var navLinksList = document.querySelector(".nav-links");
+// querySelectorAll
+var navLinks = document.querySelectorAll(".nav-links a");
+var sections = document.querySelectorAll("section");
+var filterButtons = document.querySelectorAll(".portfolio-filter");
+var portfolioItems = document.querySelectorAll(".portfolio-item");
 // ---------------------- Events ------------------------
 /*[#1]scrollSpy*/
 document.addEventListener("scroll", () => {
@@ -29,14 +34,44 @@ document.addEventListener("scroll", () => {
     }
   });
 });
-/*[#2]menu Toggle*/
+/*menu Toggle*/
 menuBTN.addEventListener("click", () => {
   navLinksList.classList.toggle("showNavLinksList");
 });
-/*[#3]theme Toggle*/
+/*[#2]theme Toggle*/
 themeToggleBTN.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
 });
+/*[#3]navs & tabs*/
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    filterButtons.forEach((btn) => btn.classList.remove(
+      "active","hover:shadow-lg","hover:shadow-primary/50","bg-linear-to-r","from-primary","to-secondary","text-white","shadow-lg","shadow-primary/50"
+    ));
+    button.classList.add(
+      "active",
+      "hover:shadow-lg","hover:shadow-primary/50","bg-linear-to-r","from-primary","to-secondary","text-white","shadow-lg","shadow-primary/50"
+    );
+    var filterValue = button.dataset.filter;
+    // items
+    portfolioItems.forEach((item) => {
+      var category = item.dataset.category;
+      if (filterValue === "all" || category === filterValue) {
+        item.style.opacity = "1";
+        item.style.transform = "scale(1)";
+        item.style.display = "block";
+      } else {
+        item.style.opacity = "0";
+        item.style.transform = "scale(0.9)";
+        setTimeout(() => {
+          item.style.display = "none";
+        }, 300);
+      }
+    });
+  });
+});
+/*[#4]carousel*/
+/*[#5]sidebar*/
 /*[#6]scroll to top*/
 // show BTN
 document.addEventListener("scroll", () => {

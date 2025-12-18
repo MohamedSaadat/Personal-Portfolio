6e8eb249fbe0a95bb1c7182b194111d5bf8818a2
MohamedSaadat <mohamedgitsaadat@gmail.com>
2025-12-18 20:18:17 +0200

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,11 +20,36 @@ var portfolioItems = document.querySelectorAll(".portfolio-item");
 var testimonialCard = document.querySelectorAll(".testimonial-card");
 var carouselIndicator = document.querySelectorAll(".carousel-indicator");
 var fontOption = document.querySelectorAll(".font-option");
+var themeColorsGrid = document.querySelectorAll(".theme-colors-grid");
+// ---------------------- localStorage ------------------------
+// theme check storage
+if (localStorage.getItem("lightAndDark") === "dark") {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+// font check storage
+if (localStorage.getItem("font") !== null) {
+  document.body.classList.remove("font-tajawal");
+  document.body.classList.add(localStorage.getItem("font"));
+} else {
+  document.body.classList.add("font-tajawal");
+}
+// color check storage
+if (localStorage.getItem("color") !== null) {
+  document.documentElement.removeAttribute("style");
+  document.documentElement.setAttribute("style", localStorage.getItem("color"));
+} else {
+  document.documentElement.setAttribute(
+    "style",
+    "--color-primary: #6366f1; --color-secondary: #8b5cf6; --color-accent: #a855f7;"
+  );
+  console.log("ok");
+}
 // ---------------------- Events ------------------------
 /*[#1]scrollSpy*/
 document.addEventListener("scroll", () => {
   var sectionId = "";
-
   // [#1] forEach() look like for loop
   // [#2] (pram) => {} arrow function (shortcut)
   sections.forEach((section) => {
@@ -35,7 +60,6 @@ document.addEventListener("scroll", () => {
       sectionId = section.getAttribute("id");
     }
   });
-
   navLinks.forEach((link) => {
     link.classList.remove("active");
 
@@ -51,6 +75,11 @@ menuBTN.addEventListener("click", () => {
 /*[#2]theme Toggle*/
 themeToggleBTN.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.setItem("lightAndDark", "dark");
+  } else {
+    localStorage.setItem("lightAndDark", "light");
+  }
 });
 /*[#3]navs & tabs*/
 filterButtons.forEach((button) => {
@@ -107,14 +136,11 @@ if (window.innerWidth <= 639) {
 } else if (window.innerWidth <= 1023) {
   w = 1;
   document.getElementById("screen2").classList.add("none");
-  console.log(window.innerWidth, w);
 } else {
   w = 2;
   document.getElementById("screen1").classList.add("none");
   document.getElementById("screen2").classList.add("none");
-  console.log(window.innerWidth, w);
 }
-// var w = window.innerWidth < 1024 ? 0 : 2;
 var totalCards = testimonialCard.length - w;
 function updateCarousel() {
   var cardWidth = testimonialCard[0].offsetWidth;
@@ -160,10 +186,10 @@ settingsToggle.addEventListener("click", () => {
   settingsSidebar.classList.remove("translate-x-full");
   settingsToggle.classList.add("right");
 });
-closeSettingsBTN.addEventListener("click",()=>{
+closeSettingsBTN.addEventListener("click", () => {
   settingsSidebar.classList.add("translate-x-full");
   settingsToggle.classList.remove("right");
-})
+});
 // change font
 fontOption.forEach((button, index) => {
   var fonts = ["font-alexandria", "font-tajawal", "font-cairo"];
@@ -179,6 +205,7 @@ fontOption.forEach((button, index) => {
       document.body.classList.add(fonts[index]);
       // for view check mark icon
       button.classList.add("active");
+      localStorage.setItem("font", fonts[index]);
     } else if (index === 1) {
       document.body.classList.remove(
         "font-alexandria",
@@ -188,6 +215,7 @@ fontOption.forEach((button, index) => {
       document.body.classList.add(fonts[index]);
       // for viwe check mark icon
       button.classList.add("active");
+      localStorage.setItem("font", fonts[index]);
     } else {
       document.body.classList.remove(
         "font-alexandria",
@@ -197,13 +225,49 @@ fontOption.forEach((button, index) => {
       document.body.classList.add(fonts[index]);
       // for viwe check mark icon
       button.classList.add("active");
+      localStorage.setItem("font", fonts[index]);
     }
   });
 });
 // change color
-
+themeColorsGrid.forEach((button, index) => {
+  var colors = [
+    "--color-primary: #6366f1; --color-secondary: #8b5cf6; --color-accent: #a855f7;",
+    "--color-primary: #ec4899; --color-secondary: #f97316; --color-accent: #fb923c;",
+    "--color-primary: #10b981; --color-secondary: #059669; --color-accent: #34d399;",
+    "--color-primary: #3b82f6; --color-secondary: #06b6d4; --color-accent: #22d3ee;",
+    "--color-primary: #ef4444; --color-secondary: #f43f5e; --color-accent: #fb7185;",
+    "--color-primary: #f59e0b; --color-secondary: #ea580c; --color-accent: #fbbf24;",
+  ];
+  button.addEventListener("click", () => {
+    themeColorsGrid.forEach((btn) =>
+      btn.classList.remove(
+        "ring-2",
+        "ring-primary",
+        "ring-offset-2",
+        "ring-offset-white",
+        "dark:ring-offset-slate-900"
+      )
+    );
+    for (var i = 0; i < colors.length; i++) {
+      if (index === i) {
+        document.documentElement.style.cssText = colors[i];
+        localStorage.setItem("color", colors[i]);
+        button.classList.add(
+          "ring-2",
+          "ring-primary",
+          "ring-offset-2",
+          "ring-offset-white",
+          "dark:ring-offset-slate-900"
+        );
+      }
+    }
+  });
+});
 // reset settings
 resetSettings.addEventListener("click", () => {
+  localStorage.setItem("lightAndDark", "dark");
+  localStorage.setItem("font", "font-tajawal");
   window.location.reload();
 });
 /*[#6]scroll to top*/
